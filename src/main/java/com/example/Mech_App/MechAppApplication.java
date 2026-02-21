e2fda4acbbc5fc1aecc98c609a4ff15fa2a277bd
TudorAlexandruMarian <109518415+TudorAlexandruMarian@users.noreply.github.com>
2026-02-21 19:14:43 +0200

    firt push

---
+++ src/main/java/com/example/Mech_App/MechAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mech_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MechAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Mech_App/MechAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mech_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MechAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
